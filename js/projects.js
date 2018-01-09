3c3ff36aaa9beb9126c42e368f98d510e05e94ea
Mishal Shah <shahmishal1998@gmail.com>
2018-01-10 02:03:18 +0530

    updated projects page

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
-    var colours=["#000","#66D9EF","#A6E22E","#272822","#FD971F"];
+    var colours=["#000","#66D9EF","#A6E22E","#FD971F","#fff","#F92672"];
     var i;
     var past_list=$('.carousel-indicators.past');
     var ongoing_list=$('.carousel-indicators.ongoing');
@@ -10,22 +10,18 @@
     past_items.empty();
     ongoing_items.empty();
 
-    var $new_background_slideshow = $("#slideshow");
-    var $new_background_body = $("body");
-
-    var random = Math.floor((Math.random() * colours.length) + 1);
-    $new_background_body.css("background-color",colours[random]);
-    $new_background_body.css("background-color",colours[random]);
     var ongoing_projects_count=0;
     var past_projects_count=0;
     for(i in projects){
+        var random = Math.floor((Math.random() * colours.length) + 1);
+
         if(projects[i].status==="past"){
             if(past_projects_count===0){
                 past_list.append('<li data-target="#pastprojects" data-slide-to="0" class="active"></li>');
 
                 past_items.append('<div class="item active">\
                                     <div class="container mypy py-3">\
-                                        <div class="card">\
+                                        <div class="card" style="background-color:'+colours[ Math.floor((Math.random() * colours.length))]+'">\
                                           <div class="row ">\
                                             <div class="col-md-4 mobile">\
                                               <img src="../images/img_avatar1.png" class="w-100" style="border-radius: 10px;">\
@@ -55,7 +51,7 @@
 
                 past_items.append('<div class="item">\
                                       <div class="container mypy py-3">\
-                                        <div class="card">\
+                                        <div class="card" style="background-color:'+colours[ Math.floor((Math.random() * colours.length))]+'">\
                                           <div class="row ">\
                                             <div class="col-md-4 mobile">\
                                               <img src="../images/img_avatar1.png" class="w-100" style="border-radius: 10px;">\
@@ -88,7 +84,7 @@
 
                 ongoing_items.append('<div class="item active">\
                 <div class="container mypy py-3">\
-                  <div class="card">\
+                  <div class="card" style="background-color:'+colours[ Math.floor((Math.random() * colours.length))]+'">\
                     <div class="row ">\
                       <div class="col-md-4 mobile">\
                         <img src="../images/img_avatar1.png" class="w-100" style="border-radius: 10px;">\
@@ -119,7 +115,7 @@
 
                 ongoing_items.append('<div class="item">\
                 <div class="container mypy py-3">\
-                    <div class="card">\
+                    <div class="card" style="background-color:'+colours[ Math.floor((Math.random() * colours.length))]+'">\
                       <div class="row ">\
                           <div class="col-md-4 mobile">\
                             <img src="../images/img_avatar1.png" class="w-100" style="border-radius: 10px;">\
