05df47ff155f0dd89847c71e0d97ceff5f54bec8
Praveen Gupta <pvgupta24@gmail.com>
2018-01-09 20:05:31 +0530

    Team page updated

--- js/team.js
+++ js/team.js
@@ -80,10 +80,10 @@ $(function(){
                 .append(block(person[fieldNames['linkedin']],'linkedin'))
                     .append($('<span></span>',{'html':'&emsp;'}));
     };
-    var $makeCard = function(person){ 
-        return $('<div></div>',{'class':'contestLink',})
-                    .append($('<div></div>',{'class':'contestsSpace',})
-                            .append($('<div></div>',{'class':'contestTitle','html':person[fieldNames['name']]})
+    var $makeCard = function(person,i,j){ 
+        return $('<div></div>',{'class':'contestLink','id':'contestLink'+i+j})
+                    .append($('<div></div>',{'class':'contestsSpace','id':'contestSpace'+i+j})
+                            .append($('<div></div>',{'class':'contestTitle','id':'contestTitle'+i+j,'html':person[fieldNames['name']]})
                                 .append(profile(person))
                             )
                     );            
@@ -102,10 +102,27 @@ $(function(){
         $contests.append($('<div></div>',{'id':'cardGroup'+index,'class':'cardGroup'}));
 
         $.each(jsonVariables[index],function(personId,person){
-                $('#cardGroup'+index).append($makeCard(person));
+                $('#cardGroup'+index).append($makeCard(person,index,personId));
             });
     });
     
+    $.each(jsonVariables[0],function(id,person){
+        var $imgBox = $('<span></span>',{
+                        'class':'imgBox',
+                        }).append($('<img/>',{
+                                'src':person[fieldNames['image']],
+                                'class':'img'
+                            }));
+        var $postBox = $('<div></div>',{
+                        'class':'details',
+                        'html':person[fieldNames['post']],                
+                        });
+        $('#contestSpace0'+id).prepend($imgBox);
+        $('#contestSpace0'+id).append($postBox);
+        
+    });
+
+
     //Default State
     var reset = function(){
         $('#tab0').addClass('active');
@@ -115,11 +132,26 @@ $(function(){
     reset();
 
     $('.tab').click(function(){
-        var $tab = $('#'+$(this).attr('id'));
-        var $group = $('#'+$(this).attr('data-target'));
+        var tabId = '#'+$(this).attr('id');
+        var groupId = '#'+$(this).attr('data-target');
+
         $('.active').removeClass('active');
-        $tab.addClass('active');
-        $('.cardGroup').slideUp(500);//.hide();
-        $group.slideDown(500);//.show();        
+        $(tabId).addClass('active');
+
+        var $imgBox =$('.imgBox');
+        if(tabId=='#tab0')
+            $imgBox.fadeIn(1200);            
+        else  
+            $imgBox.fadeOut(200);
+
+        $('.cardGroup:not('+groupId+')').hide();//.hide();
+        $(groupId).slideDown(500);//.show();    
+    
+        var $imgBox =$('.imgBox');
+        if(tabId=='#tab0')
+            $imgBox.show(1800);            
+        else  
+            $imgBox.fadeOut(200);
+
     });    
 });
